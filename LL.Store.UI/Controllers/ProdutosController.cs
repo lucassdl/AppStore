56e14920e8d20e4f0fedda5026c69a93b2c39ee6
Lucas Lima <lucas_sdl@hotmail.com>
2019-08-12 22:40:18 -0300

    Add project files.

---
+++ LL.Store.UI/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using LL.Store.UI.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace LL.Store.UI.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ViewResult Index()
+        {
+            var produtos = new List<Produto>()
+            {
+                new Produto(){Id=1,Nome="Picanha",Preco=70.5m,Qtde=150, Tipo="Alimento"},
+                new Produto(){Id=2,Nome="Pasta de Dente",Preco=9.5m,Qtde=250, Tipo="Higiene"},
+                new Produto(){Id=3,Nome="Desinfetante",Preco=8.99m,Qtde=520, Tipo="Limpeza"}
+            };
+
+            return View(produtos);
+        }
+    }
+}
---
+++ LL.Store.UI/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LL.Store.UI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Tipo { get; set; }
+        public Int16 Qtde { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
