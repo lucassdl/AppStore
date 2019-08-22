0142273048b797487de8412a7efef52b8f4a03e9
Lucas Lima <lucas_sdl@hotmail.com>
2019-08-21 23:53:45 -0300

    Utilização da classe XmlHttpRequset para fazer exlusão de dados de forma assíncrona

--- LL.Store.UI/Controllers/ProdutosController.cs
+++ LL.Store.UI/Controllers/ProdutosController.cs
@@ -1,36 +1,77 @@
 ﻿using LL.Store.UI.Data;
+using LL.Store.UI.Models;
 using System.Linq;
 using System.Web.Mvc;
-using LL.Store.UI.Models;
-using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace LL.Store.UI.Controllers
 {
     public class ProdutosController : Controller
     {
-		[HttpGet]
+        private readonly LNStoreDataContext _ctx = new LNStoreDataContext();
+
+        [HttpGet]
         public ViewResult Index()
         {
-            IList<Produto> produtos = null;
-
-            using (var ctx = new LNStoreDataContext())
-            {
-                produtos = ctx.Produtos.ToList();
-            }
+            var produtos = _ctx.Produtos.ToList();
 
             return View(produtos);
         }
 
-		[HttpGet]
-		public ViewResult Add()
+        [HttpGet]
+        public ViewResult AddEdit(int? id)
         {
-            return View();
+            Produto produto = null;
+
+            if (id != null)
+                produto = _ctx.Produtos.Find(id);
+
+            return View(produto);
         }
 
-		[HttpPost]
-		public ActionResult Add(Produto produto)
+        [HttpPost]
+        public ActionResult AddEdit(Produto produto)
         {
+            //TODO: Validar
+
+            if (produto.Id == 0)
+                _ctx.Produtos.Add(produto);
+            else
+                _ctx.Entry(produto).State = EntityState.Modified;
+
+            _ctx.SaveChanges();
+
             return RedirectToAction("Index");
         }
+
+        public ViewResult Edit(int id)
+        {
+            var produto = _ctx.Produtos.Find(id);
+            return View(produto);
+        }
+
+        public ActionResult DelProduto(int id)
+        {
+            var produto = _ctx.Produtos.Find(id);
+
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                _ctx.Produtos.Remove(produto);
+                _ctx.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+        }
+
+
     }
 }
